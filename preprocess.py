a97ef108aae34877ba7e79a87abe92994f8917a0
filip <fzietara2137@gmail.com>
2019-10-10 23:24:03 +0200

    get_rid_of_scores_lower_than_10 now works properly

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 df = pd.read_csv("googledata.csv")
 
 df = df[['subject_1',
@@ -12,6 +13,10 @@ df = df[['subject_1',
          'hours_of_sleep',
          'study_time',
          'stress_level']]
+
+
+def is_nan(num):
+    return num != num
 
 
 def get_rid_of_percent_symbol(x):  # some inputs were e.g '90%', and we need the value rather than some percentage
@@ -34,8 +39,7 @@ def get_rid_of_ranges(x):  # some inputs were like '50-80', this function will g
 
 
 def get_rid_of_scores_lower_than_10(x):  # some inputs were like '5',I assume it was meant to be '50' and multiply by 10
-    x = pd.Series([x])
-    if x.isnull() is False:
+    if x != 'nan':
         x = int(x)
         if x < 10:
             return x*10
@@ -46,7 +50,7 @@ def get_rid_of_scores_lower_than_10(x):  # some inputs were like '5',I assume it
 
 
 for i in range(1, 4):
-    df['score_' + str(i)] = df.apply(lambda row: get_rid_of_percent_symbol(row['score_' +str(i)]), axis=1)
+    df['score_' + str(i)] = df.apply(lambda row: get_rid_of_percent_symbol(row['score_' + str(i)]), axis=1)
     df['score_' + str(i)] = df.apply(lambda row: get_rid_of_ranges(row['score_' + str(i)]), axis=1)
     df['score_' + str(i)] = df.apply(lambda row: get_rid_of_scores_lower_than_10(row['score_' + str(i)]), axis=1)
 
