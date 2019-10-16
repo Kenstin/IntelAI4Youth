5e972e904f5dc0c5a68edd053fe33f59c6822a6c
filip <fzietara2137@gmail.com>
2019-10-16 17:51:57 +0200

    preprocess.py gets rid of nans now
    we had to get rid of nans because our model seemed to freak out

--- preprocess.py
+++ preprocess.py
@@ -43,6 +43,8 @@ def get_rid_of_scores_lower_than_10(x):  # some inputs were like '5',I assume it
             return x
     else:
         return x
+
+
 def get_rid_of_and_more(x):
     if x == '5 lub wiecej':
         x = 5
@@ -50,11 +52,19 @@ def get_rid_of_and_more(x):
     else:
         return x
 
+
+def get_rid_of_nans(x):
+    if x == 'nan':
+        return 0
+    else:
+        return x
+
+
 for i in range(1, 4):
     df['score_' + str(i)] = df.apply(lambda row: get_rid_of_percent_symbol(row['score_' + str(i)]), axis=1)
     df['score_' + str(i)] = df.apply(lambda row: get_rid_of_ranges(row['score_' + str(i)]), axis=1)
     df['score_' + str(i)] = df.apply(lambda row: get_rid_of_scores_lower_than_10(row['score_' + str(i)]), axis=1)
-
+    df['score_' + str(i)] = df.apply(lambda row: get_rid_of_nans(row['score_' + str(i)]), axis=1)
 
 df['coffees'] = df.apply(lambda row: get_rid_of_and_more(row['coffees']), axis=1)
 df['energy_drinks'] = df.apply(lambda row: get_rid_of_and_more(row['energy_drinks']), axis=1)
