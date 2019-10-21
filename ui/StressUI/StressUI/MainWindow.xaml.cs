4e960b8b0fe9c4c6f4d7835e4616bcb3697f1f84
Unknown <k3nstin@gmail.com>
2019-10-21 19:16:46 +0200

    Mahtura bootstrap with simple layout

---
+++ ui/StressUI/StressUI/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ReactiveUI;
+
+namespace StressUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ReactiveWindow<StressViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ui/StressUI/StressUI/StressViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ReactiveUI;
+
+namespace StressUI
+{
+    public class StressViewModel : ReactiveObject
+    {
+        
+    }
+}
